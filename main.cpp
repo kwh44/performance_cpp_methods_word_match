81caa202adfadeee22bb822312981b1cfe4fa659
kwh44 <maistruk44@gmail.com>
2019-03-18 20:05:53 +0200

    removed time-measure code duplication

--- main.cpp
+++ main.cpp
@@ -21,9 +21,21 @@ inline long long to_us(const D &d) {
     return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
 }
 
-
 int get_method_id(const char *id) {
-    return *id - 48;
+    return *id - 48; // ascii table
+}
+
+void run_method_by_id(std::ifstream & file, int method_id, unsigned & result) {
+    if (method_id == 1) {
+        // cpp idiom file >> word
+        result = cpp_word_file_idiom(file);
+    } else if (method_id == 2) {
+        // tristan tokenizing algorithm
+        result = tristan_algorithm(file);
+    } else if (method_id == 3) {
+        // boost tokenizer
+        result = boost_algorithm(file);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -31,31 +43,15 @@ int main(int argc, char *argv[]) {
     // id of method to use argv[1]
     // file_name argv[2]
     // output_file_name argv[3]
-    int method_id = get_method_id(argv[1]);
     std::string filename = argv[2];
     std::ifstream file(argv[2]);
     std::ofstream output_file(argv[3], std::ofstream::out);
     unsigned count = 0;
     long long total_time = 0;
-    if (method_id == 1) {
-        // cpp idiom file >> word
-        auto start_time = get_current_time_fenced();
-        count = cpp_word_file_idiom(file);
-        auto finish_time = get_current_time_fenced();
-        total_time = to_us(finish_time - start_time);
-    } else if (method_id == 2) {
-        // tristan tokenizing algorithm
-        auto start_time = get_current_time_fenced();
-        count = tristan_algorithm(file);
-        auto finish_time = get_current_time_fenced();
-        total_time = to_us(finish_time - start_time);
-    } else if (method_id == 3) {
-        // boost tokenizer
-        auto start_time = get_current_time_fenced();
-        count = boost_algorithm(file);
-        auto finish_time = get_current_time_fenced();
-        total_time = to_us(finish_time - start_time);
-    }
+    auto start_time = get_current_time_fenced();
+    run_method_by_id(file, get_method_id(argv[1]), count);
+    auto finish_time = get_current_time_fenced();
+    total_time = to_us(finish_time - start_time);
     std::cout << total_time << std::endl;
     output_file << count << std::endl;
     file.close();
